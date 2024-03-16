cd377d77f62338c11c9912b874b80cc7ce271d9d
aidenhigginbotham-pvcc <160060555+aidenhigginbotham-pvcc@users.noreply.github.com>
2024-03-15 22:56:44 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,31 @@
+# Name: Aiden Higginbotham
+# Program Purpose: Hello World in Python
+
+print("Hello World! This is my frist Python program.")
+print("I can program in HTML, whic is awesome.")
+print("However, HTML cannot do arithmetic.")
+print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots of other things!")
+
+
+print('I love Python!')
+print("\n\t\\Hello\\ \n\t\tWorld!")
+print("This is some work I did in class when first learning Python")
+#Python can do arithmetic too.
+aiden = 20
+albert = 19
+drcstevens = 68
+sumages = aiden + albert + drcstevens
+ave = sumages / 3
+print("The average age is:",ave)
+
+
+
+#Next year:
+aiden += 10
+albert += 10
+drcstevens += 10
+sumages = aiden + albert + drcstevens
+ave = sumages / 3
+print("Aiden next year:", aiden)
+print("Next year, the average age will be:",ave)
